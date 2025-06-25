69bcc74ae258c62487dc861c518d2fca222b59b3
RR <83947701+rimimakita@users.noreply.github.com>
2025-06-25 15:42:08 +0900

    Update server.py

--- server.py
+++ server.py
@@ -26,7 +26,7 @@ pipe_lock = threading.Lock()
 caption_processor = None
 caption_model = None
 pipe = None
-keywords = ["text", "numbers", "logo","wallpaper"]
+keywords = ["text", "numbers", "logo","wall"]
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 dtype = torch.float16
